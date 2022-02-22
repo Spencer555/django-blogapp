52668954937c3636040c475e939f23af3bb1324e
Spencer555 <46141953+Spencer555@users.noreply.github.com>
2022-02-22 01:03:36 +0000

    Update settings.py

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 # DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [localhost, ".herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
